51c3e646260c4bddeab45784aaeaac6f3fc83749
tomasbures93 <98662129+tomasbures93@users.noreply.github.com>
2025-04-22 20:15:34 +0200

    GeoCharts done, was kinda fun

--- ApplyLog/Controllers/HomeController.cs
+++ ApplyLog/Controllers/HomeController.cs
@@ -25,7 +25,6 @@ namespace ApplyLog.Controllers
         {
             if (string.IsNullOrEmpty(search))
             {
-                //Tuple<List<TODO>, List<Bewerbung>> data = new Tuple<List<TODO>, List<Bewerbung>>(null, null);
                 return PartialView("_formSearch", null);
             }
             List<TODO> todoList = appDbContext.Todos.Where(s => s.Titel.ToLower().Contains(search.ToLower())).ToList();
@@ -43,6 +42,12 @@ namespace ApplyLog.Controllers
                 new { Label = "High", Count = appDbContext.Todos.Where(p => p.PriorityLevel == PriorityLevel.High).Count() }
             };
             return PartialView("_chartTodo", dataForChart);
+        }
+
+        public PartialViewResult GeoChart()
+        {
+            List<string> cities = appDbContext.Applications.Select(c => c.jobort).Distinct().ToList();
+            return PartialView("_geoChart", cities);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
